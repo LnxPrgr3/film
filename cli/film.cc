d3549f7a6f17e8bd29cc9e2e35d6d91629f41f10
Jeremy Pepper <jpepper1@gmail.com>
2020-04-17 23:20:36 -0500

    Add basic photographic print model

--- cli/film.cc
+++ cli/film.cc
@@ -12,14 +12,15 @@ struct options {
 	const char *title;
 	float gamma;
 	float fog;
+	float print_contrast;
 
-	options() : title("film"), gamma(1.5f), fog(1.f / 512.f) {}
+	options() : title("film"), gamma(1.5f), fog(1.f / 512.f), print_contrast(-1) {}
 };
 
 static options parse_options(int argc, char *argv[]) {
 	options result;
 	int ch;
-	while ((ch = getopt(argc, argv, "t:g:f:")) != -1) {
+	while ((ch = getopt(argc, argv, "t:g:f:p:")) != -1) {
 		switch (ch) {
 		case 't':
 			result.title = optarg;
@@ -30,6 +31,9 @@ static options parse_options(int argc, char *argv[]) {
 		case 'f':
 			result.fog = 1. / pow(2., strtod(optarg, NULL));
 			break;
+		case 'p':
+			result.print_contrast = strtod(optarg, NULL);
+			break;
 		}
 	}
 	return result;
@@ -37,7 +41,21 @@ static options parse_options(int argc, char *argv[]) {
 
 int main(int argc, char *argv[]) {
 	const options options = parse_options(argc, argv);
-	const film film(CIERGB_colorspace, options.gamma, options.fog);
 
-	lut_write(cout, options.title, 64, film);
+	if (options.print_contrast > 0) {
+		float stage_gamma = sqrt(options.gamma);
+		const film film(CIERGB_colorspace, stage_gamma, options.fog);
+		rgb white = film(film({1.f, 1.f, 1.f}));
+		float white_level =
+		    (gamma_2_2_transfer.decode(white.r) + gamma_2_2_transfer.decode(white.g) +
+		     gamma_2_2_transfer.decode(white.b)) /
+		    3;
+		const class film print(CIERGB_colorspace, stage_gamma,
+		                       white_level / options.print_contrast);
+
+		lut_write(cout, options.title, 64, [&](rgb x) { return print(film(x)); });
+	} else {
+		const film film(CIERGB_colorspace, options.gamma, options.fog);
+		lut_write(cout, options.title, 64, film);
+	}
 }
