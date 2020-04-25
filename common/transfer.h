2508e4e1e858e61a17b2ecf1c62b92a1cb68a705
Jeremy Pepper <jpepper1@gmail.com>
2020-04-25 15:55:17 -0500

    Make transfer functions work on pixels

--- common/transfer.h
+++ common/transfer.h
@@ -1,10 +1,23 @@
 #ifndef FILM_COMMON_TRANSFER_H
 #define FILM_COMMON_TRANSFER_H
+#include "types.h"
 
 struct transfer_function {
 	virtual ~transfer_function();
 	virtual float decode(const float) const = 0;
 	virtual float encode(const float) const = 0;
+
+	pixel decode(const pixel &x) const {
+		return {decode(x.subpixels[0]), decode(x.subpixels[1]), decode(x.subpixels[2])};
+	}
+
+	pixel encode(const pixel &x) const {
+		return {
+		    encode(x.subpixels[0]),
+		    encode(x.subpixels[1]),
+		    encode(x.subpixels[2]),
+		};
+	}
 };
 
 class gamma : public transfer_function {
@@ -16,12 +29,16 @@ public:
 	constexpr gamma(float gamma) : _gamma(gamma), _inv_gamma(1 / gamma) {}
 	float decode(const float) const;
 	float encode(const float) const;
+	using transfer_function::decode;
+	using transfer_function::encode;
 };
 
 class linear_transfer_function : public transfer_function {
 public:
 	float decode(const float) const;
 	float encode(const float) const;
+	using transfer_function::decode;
+	using transfer_function::encode;
 };
 
 extern const class gamma gamma_2_2_transfer;
